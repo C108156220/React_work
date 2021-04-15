b80f3e70441abda8759e209e901d98125660a788
C108156220 <C108156220@nkust.edu.tw>
2021-04-16 02:15:36 +0800

    Third test

--- 0415/index.js
+++ 0415/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './App';
 
-function tick() {
-  ReactDOM.render(
-    <Clock date={new Date()} />,
-    document.getElementById('root')
-  );
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
