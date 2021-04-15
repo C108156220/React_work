6e2ad65a920cb53e7ed04528293c61919ec82e2c
C108156220 <C108156220@nkust.edu.tw>
2021-04-16 01:59:40 +0800

    Two files  Second test

--- 0415/index.js
+++ 0415/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
+import Clock from './App';
 
 function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
   ReactDOM.render(
-    element,
+    <Clock date={new Date()} />,
     document.getElementById('root')
   );
 }
 
 setInterval(tick, 1000);
-
-/*
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
-*/
